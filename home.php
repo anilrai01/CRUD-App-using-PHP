84f6029475e941f43d77176430004e3c43e41c0d
anil.rai01 <anil.rai01041998@gmail.com>
2019-07-10 00:58:49 +0545

    Renamed Buttons Timeline

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
+
     <link rel="stylesheet" href="css/loggedStyle.css">
     <title>Welcome</title>
   </head>
@@ -34,8 +34,8 @@
 
                 <div class="togglers">
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                        <button class = "ownBtn" name = "postFeed"> Share Feelings </button>
-                        <button class = "ownBtn" name = "memories"> My Feeds </button>
+                        <button class = "ownBtn" name = "memories"> Timeline </button>
+                        <button class = "ownBtn" name = "postFeed"> Share Feeds </button>
                         <button class = "ownBtn" name = "profile"> Update Profile </button>
                         <button class = "ownBtn" name = "logout"> Log Out </button>
                     </form>
